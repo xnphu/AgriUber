55a54e913a7b2d87da2b500d1a1cbec2113f4997
xnphu <nguyenxuanphu188@gmail.com>
2021-03-29 12:20:01 +0700

    .

---
+++ AgriUber/RoundButton.swift
@@ -0,0 +1,21 @@
+//
+//  RoundButton.swift
+//  AgriUber
+//
+//  Created by Nguyen Xuan Phu on 3/24/21.
+//  Copyright © 2021 Nguyen Xuan Phu. All rights reserved.
+//
+
+import UIKit
+
+class RoundButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
